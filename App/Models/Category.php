2db2b16239a617cf3763029e618902eb684052b2
TheChosenTwice <hurtos.tom@gmail.com>
2026-01-24 00:09:26 +0100

    Implemented category model and created dedicated controller.

---
+++ App/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Framework\Core\Model;
+
+class Category extends Model
+{
+
+    protected ?int $id = null;
+    protected string $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+}

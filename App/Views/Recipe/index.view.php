9c6554beb7c436c9723eed6bbcdc4b1eec2a93a2
TheChosenTwice <hurtos.tom@gmail.com>
2026-01-24 19:47:32 +0100

    Initial layout

--- App/Views/Recipe/index.view.php
+++ App/Views/Recipe/index.view.php
@@ -1,6 +1,21 @@
 <?php
 /** @var \Framework\Support\LinkGenerator $link */
-
+/** @var array $recipes */
 ?>
 
-This is recipe index page.
+<h1>Recipe Results</h1>
+
+<?php if (empty($recipes)) : ?>
+    <p>No recipes found.</p>
+<?php else: ?>
+    <?php foreach ($recipes as $recipe) : ?>
+        <li>
+            <img src="/images/<?= $recipe['image'] ?>" style="max-width:80px; max-height:100px;" alt="Recipe Image"><br>
+            <?= $recipe['title'] ?><br>
+            <strong>Cooking time:</strong> <?= $recipe['cooking_time'] ?> minutes<br>
+            <strong>Missing ingredients:</strong> <?= $recipe['num_of_ingredients'] - $recipe['num_of_ingredients_met'] ?><br>
+            <strong>Number of ingredients:</strong> <?= $recipe['num_of_ingredients'] ?><br>
+            <strong>Number of ingredients met:</strong> <?= $recipe['num_of_ingredients_met'] ?>
+        </li>
+    <?php endforeach; ?>
+<?php endif; ?>

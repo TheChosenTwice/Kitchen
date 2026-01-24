6b02f8e85a88bb46eaf5d8ff68021bbebba68daf
TheChosenTwice <hurtos.tom@gmail.com>
2026-01-24 19:24:50 +0100

    Controller will now pass recipes matching at least 1 ingredient and sorting them by most ingredients fulfilled.

--- App/Controllers/RecipeController.php
+++ App/Controllers/RecipeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\Recipe;
 use Framework\Core\BaseController;
 use Framework\Http\Request;
 use Framework\Http\Responses\Response;
@@ -11,6 +12,18 @@ class RecipeController extends BaseController
 
     public function index(Request $request): Response
     {
-        return $this->html();
+        $ingredientsIds = $request->get('ingredients');
+        if (empty($ingredientsIds))
+            return $this->html(['recipes' => []]);
+
+        $sql = "SELECT id, title, cooking_time, image, COUNT(*) AS num_of_ingredients, 
+            COUNT(CASE WHEN recipe_ingredients.ingredient_id IN ($ingredientsIds) THEN 1 END) AS num_of_ingredients_met
+            FROM recipes JOIN recipe_ingredients ON recipes.id = recipe_ingredients.recipe_id
+            GROUP BY recipes.id, recipes.title
+            HAVING num_of_ingredients_met > 0
+            ORDER BY num_of_ingredients_met DESC, num_of_ingredients";
+        $recipes = Recipe::executeRawSQL($sql);
+
+        return $this->html(['recipes' => $recipes]);
     }
 }

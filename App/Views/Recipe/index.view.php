34eedf6ec4376d5f9007dee1bb0725aa8b7066ca
TheChosenTwice <hurtos.tom@gmail.com>
2026-01-25 14:15:36 +0100

    Structural improvements and added redirecting to detailed recipe view

--- App/Views/Recipe/index.view.php
+++ App/Views/Recipe/index.view.php
@@ -1,21 +1,35 @@
 <?php
 /** @var \Framework\Support\LinkGenerator $link */
-/** @var array $recipes */
+/** @var Array $recipes */
 ?>
 
 <h1>Recipe Results</h1>
 
 <?php if (empty($recipes)) : ?>
-    <p>No recipes found.</p>
+    <p class="no-results">No recipes found.</p>
 <?php else: ?>
-    <?php foreach ($recipes as $recipe) : ?>
-        <li>
-            <img src="/images/<?= $recipe['image'] ?>" style="max-width:80px; max-height:100px;" alt="Recipe Image"><br>
-            <?= $recipe['title'] ?><br>
-            <strong>Cooking time:</strong> <?= $recipe['cooking_time'] ?> minutes<br>
-            <strong>Missing ingredients:</strong> <?= $recipe['num_of_ingredients'] - $recipe['num_of_ingredients_met'] ?><br>
-            <strong>Number of ingredients:</strong> <?= $recipe['num_of_ingredients'] ?><br>
-            <strong>Number of ingredients met:</strong> <?= $recipe['num_of_ingredients_met'] ?>
-        </li>
-    <?php endforeach; ?>
+    <ul class="recipes-grid">
+        <?php foreach ($recipes as $recipe) : ?>
+            <li class="recipe-card">
+                <div class="img">
+                    <img src="/images/<?= $recipe['image'] ?>" alt="<?= $recipe['title'] ?>">
+                </div>
+                <div class="recipe-body">
+                    <a href="<?= $link->url("recipe.recipeDetail", ["id" => $recipe['id']]) ?>">
+                        <?= $recipe['title'] ?>
+                    </a>
+                    <div class="meta"><strong>Cooking Time:</strong> <?= $recipe['cooking_time'] ?> minutes</div>
+                    <?php $missing = $recipe['num_of_ingredients'] - $recipe['num_of_ingredients_met'] ?>
+                    <div class="meta">
+                        <?php if ($missing === 0) : ?>
+                            <span class="badge">No missing ingredients!</span>
+                        <?php else: ?>
+                            <span class="badge"><?= $missing ?> missing ingredients</span>
+                        <?php endif; ?>
+                        <a class="btn" href="#">Log in to bookmark!</a>
+                    </div>
+                </div>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 <?php endif; ?>

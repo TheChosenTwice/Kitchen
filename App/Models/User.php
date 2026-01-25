97accda8225573064419be4308c7058012909406
TheChosenTwice <hurtos.tom@gmail.com>
2026-01-25 17:55:48 +0100

    Implemented User model

---
+++ App/Models/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Framework\Core\IIdentity;
+use Framework\Core\Model;
+
+class User extends Model implements IIdentity
+{
+    // DB columns must match these protected properties
+    protected ?int $id = null;
+    protected string $username;
+    protected string $password_hash;
+    protected ?string $created_at = null;
+    protected string $role = 'USER';
+
+    public function getId(): ?int { return $this->id; }
+    public function getUsername(): string { return $this->username; }
+    public function setUsername(string $username): void { $this->username = $username; }
+    public function getPasswordHash(): string { return $this->password_hash; }
+    public function getCreatedAt(): ?string { return $this->created_at; }
+    public function getRole(): string { return $this->role; }
+    public function setRole(string $role): void { $this->role = $role; }
+
+    public function setPassword(string $plain): void
+    {
+        $this->password_hash = password_hash($plain, PASSWORD_DEFAULT);
+    }
+
+    /**
+     * IIdentity getName implementation.
+     */
+    public function getName(): string { return $this->username;}
+
+    public static function findByUsername(string $username): ?User {
+        $user = User::getAll('username = ?', [$username], null, 1);
+        return $user[0] ?? null;
+    }
+}

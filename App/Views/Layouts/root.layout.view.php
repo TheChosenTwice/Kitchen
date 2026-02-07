ab9239d02a5ad5b9738acb5c090fb7c348f22d80
TheChosenTwice <hurtos.tom@gmail.com>
2026-02-07 23:10:52 +0100

    Bookmark - added Bookmarks button

--- App/Views/Layouts/root.layout.view.php
+++ App/Views/Layouts/root.layout.view.php
@@ -32,6 +32,9 @@
             <span class="navbar-text">Logged in user: <b><?= $user->getName() ?></b></span>
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
+                    <a class="nav-link" href="<?= $link->url('recipe.favorites') ?>">Bookmarks</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="<?= $link->url('auth.logout') ?>">Log out</a>
                 </li>
             </ul>

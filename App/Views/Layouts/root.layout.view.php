97695c60e3f16669f8a8d71fb5c5e1c670afc777
TheChosenTwice <hurtos.tom@gmail.com>
2026-02-08 16:31:11 +0100

    Added hamburger button

--- App/Views/Layouts/root.layout.view.php
+++ App/Views/Layouts/root.layout.view.php
@@ -1,8 +1,12 @@
 <?php
 
 /** @var string $contentHTML */
-/** @var \Framework\Auth\AppUser $user */
-/** @var \Framework\Support\LinkGenerator $link */
+/** @var AppUser $user */
+/** @var LinkGenerator $link */
+
+use Framework\Auth\AppUser;
+use Framework\Support\LinkGenerator;
+
 ?>
 <!DOCTYPE html>
 <html lang="sk">
@@ -26,21 +30,40 @@
 <nav class="navbar navbar-expand-sm bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="<?= $link->url('home.index') ?>">
-            <img src="<?= $link->asset('images/vaiicko_logo.png') ?>" title="<?= App\Configuration::APP_NAME ?>" alt="Framework Logo">
+            <img src="<?= $link->asset('images/vaiicko_logo.png') ?>" title="<?= App\Configuration::APP_NAME ?>"
+                 alt="Framework Logo">
         </a>
         <?php if ($user->isLoggedIn()) { ?>
-            <span class="navbar-text">Logged in user: <b><?= $user->getName() ?></b></span>
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="<?= $link->url('recipe.favorites') ?>">Bookmarks</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?= $link->url('recipe.create') ?>">New Recipe</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?= $link->url('auth.logout') ?>">Log out</a>
-                </li>
-            </ul>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <!-- Hamburger button -->
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarContent"
+                            aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <!-- Collapsable content -->
+                    <div class="collapse navbar-collapse" id="navbarContent">
+                        <span class="navbar-text me-auto">
+                            Logged in user: <b><?= $user->getName() ?></b>
+                        </span>
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?= $link->url('recipe.favorites') ?>">Bookmarks</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?= $link->url('recipe.create') ?>">New Recipe</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?= $link->url('recipe.owned') ?>">Your Recipes</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?= $link->url('auth.logout') ?>">Log out</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         <?php } else { ?>
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
